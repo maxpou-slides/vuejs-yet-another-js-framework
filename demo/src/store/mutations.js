3b0b0b60b5f98d4fffc76cc9daf76204033616ab
Maxence POUTORD <maxence.poutord@gmail.com>
2017-12-04 16:13:38 +0000

    add demo + component definition

---
+++ demo/src/store/mutations.js
@@ -0,0 +1,49 @@
+export const STORAGE_KEY = 'todos-vuejs'
+
+// for testing
+if (navigator.userAgent.indexOf('PhantomJS') > -1) {
+  window.localStorage.clear()
+}
+
+export const state = {
+  todos: [
+    {
+      done: true,
+      text: 'Learn Vue.js'
+    }, {
+      done: false,
+      text: 'Give a good talk about it'
+    }
+  ]
+}
+
+export const mutations = {
+  addTodo (state, { text }) {
+    state.todos.push({
+      text,
+      done: false
+    })
+  },
+
+  deleteTodo (state, { todo }) {
+    state.todos.splice(state.todos.indexOf(todo), 1)
+  },
+
+  toggleTodo (state, { todo }) {
+    todo.done = !todo.done
+  },
+
+  editTodo (state, { todo, value }) {
+    todo.text = value
+  },
+
+  toggleAll (state, { done }) {
+    state.todos.forEach((todo) => {
+      todo.done = done
+    })
+  },
+
+  clearCompleted (state) {
+    state.todos = state.todos.filter(todo => !todo.done)
+  }
+}
